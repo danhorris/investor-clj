98638a380fbdda625e31cec5f7f10aaa7d3ee969
Daniel Horrisberger <danhorris@gmail.com>
2026-03-27 08:56:35 -0300

    ahora leo variables

--- src/play/telegram.clj
+++ src/play/telegram.clj
@@ -1,11 +1,13 @@
 (ns play.telegram
   (:require [clj-http.client :as http]))
 
-(def telegram-token "8744872293:AAH3ZEhtfGJU5aWDXuLv9uBPnDYs1rYTQ0A")
-(def chat-id "897726568")
+(def telegram-token (System/getenv "TELEGRAM_TOKEN"))
+(def chat-id (System/getenv "CHAT_ID"))
+
 
 (defn send-telegram [text]
   (http/post
    (str "https://api.telegram.org/bot" telegram-token "/sendMessage")
    {:form-params {:chat_id chat-id
                   :text text}}))
+
