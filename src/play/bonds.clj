ae065e629d2066443744ec37604b6830f7163b46
Daniel Horrisberger <danhorris@gmail.com>
2026-03-30 08:54:22 -0300

    chequeo si no existe algun valor para los bonos en el calculo del ratio

--- src/play/bonds.clj
+++ src/play/bonds.clj
@@ -1,6 +1,7 @@
 (ns play.bonds
   (:require
-   [clj-http.client :as http]))
+   [clj-http.client :as http]
+   [clojure.set :refer [difference]]))
 
 (def url-bonds "https://data912.com/live/arg_bonds")
 (def url-cedears "https://data912.com/live/arg_cedears")
@@ -36,26 +37,47 @@
                       (historical-prices! bond))))))
 (defn ratios [a b]
   (->> (historic-data-for! a b)
+       ;;(sort-by :date)
        (group-by :date)
        (map (fn [[date xs]]
               (let [prices (into {} (map (juxt :bond :c) xs))
-                    a   (get prices a)
-                    b   (get prices b)]
-                {:date date
-                 :ratio (/ a b)})))))
+                    price-a   (get prices a)
+                    price-b   (get prices b)]
+                (when (and price-a price-b (not (zero? price-b)))
+                  {:date date
+                   :ratio (/ price-a price-b)}))))))
 
 
 (comment
   (bond-ratio! all-prices! "AE38" "GD41")
   (historical-prices! "AE38")
   (historical-prices! "GD41")
-  (historic-data-for! "AE38" "GD41")
+  (count (historical-prices! "AE38"))
+  (count (historical-prices! "GD41"))
+  (->> (historical-prices! "AE38")
+       (map :date)
+       (into #{}))
   (ratios "AE38" "GD41")
+  (last (ratios "AE38" "GD41"))
   (ratios "AE38" "GD35")
   (sort-by :date (ratios "AE38" "GD41"))
   (group-by :date (historic-data-for! "AE38" "GD41"))
   (sort-by :date (historic-data-for! "AE38" "GD41")))
 
 
+(def dates-AE38 (->> (historical-prices! "AE38")
+                     (map :date)
+                     (sort-by :date)
+                     (into #{})))
 
+(def dates-GD41 (->> (historical-prices! "GD41")
+                     (map :date)
+                     (sort-by :date)
+                     (into #{})))
+
+
+(first dates-AE38)
+(first dates-GD41)
+(difference dates-AE38 dates-GD41)
+;;falta ordenar la lista de ratios, posiblemente hay algun valor nulo
 
